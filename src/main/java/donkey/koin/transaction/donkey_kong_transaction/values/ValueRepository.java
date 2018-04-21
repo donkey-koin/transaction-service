0f59d2a9c48d664492380294f565fa3acbfa0e01
sowakarol <karosowa@student.agh.edu.pl>
2018-04-21 13:35:22 +0200

    Update Scheduled task to be cron expression, add searching for values dependent on date

--- src/main/java/donkey/koin/transaction/donkey_kong_transaction/values/ValueRepository.java
+++ src/main/java/donkey/koin/transaction/donkey_kong_transaction/values/ValueRepository.java
@@ -2,13 +2,15 @@ package donkey.koin.transaction.donkey_kong_transaction.values;
 
 import donkey.koin.transaction.donkey_kong_transaction.values.value.Value;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.lang.Nullable;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface ValueRepository extends MongoRepository<Value, String> {
-    List<Value> findByDate(LocalDateTime date);
+public interface ValueRepository extends MongoRepository<Value, String> {  //, CrudRepository<Value,String> {
+    @Nullable
+    List<Value> findAllByDate(LocalDateTime date);
 
     Optional<Value> findById(String id);
 }
