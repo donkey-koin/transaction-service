7af17164c1b814f555e04aa1ebf84a33572498f1
Siorek, Oskar <oskar.siorek@sabre.com>
2018-06-16 23:59:32 +0200

    make rsa

--- src/main/java/donkey/koin/transaction/donkey_kong_transaction/inprogres/TxHandler.java
+++ src/main/java/donkey/koin/transaction/donkey_kong_transaction/inprogres/TxHandler.java
@@ -3,7 +3,11 @@ package donkey.koin.transaction.donkey_kong_transaction.inprogres;
 import donkey.koin.transaction.donkey_kong_transaction.entities.UTXO;
 import donkey.koin.transaction.donkey_kong_transaction.repo.UTXORepository;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
@@ -12,6 +16,7 @@ public class TxHandler {
 
     private UTXORepository utxoRepository;
     private double totalInputSum;
+
     /**
      * Creates a public ledger whose current UTXOService (collection of unspent transaction outputs) is
      * {@code utxoService}. This should make a copy of utxoService by using the UTXOService(UTXOService uPool)
@@ -29,42 +34,47 @@ public class TxHandler {
      * (3) no UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
-     *     values; and false otherwise.
+     * values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
         this.totalInputSum = 0;
         return validateRuleNumber12And3(tx) &&
-               validateRuleNumber4And5(tx);
+                validateRuleNumber4And5(tx);
     }
 
     private boolean validateRuleNumber12And3(Transaction tx) {
         HashMap<UTXO, Boolean> usedUTXO = new HashMap<>();
 
-        for (int i = 0;  i < tx.numInputs(); i++) {
+        for (int i = 0; i < tx.numInputs(); i++) {
             Transaction.Input input = tx.getInput(i);
-            if (input == null) { return false; }
+            if (input == null) {
+                return false;
+            }
 
             UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
             //rule number 1
             Optional<UTXO> oUtxo = utxoRepository.findByTxHash(input.prevTxHash);
             if (!oUtxo.isPresent()) {
-              return false;
+                return false;
             }
 
-//            PublicKey publicKey = oUtxo.get().getAddress();
-//            if (publicKey == null) {
-//                return false;
-//            }
+            PublicKey publicKey = getPublicKeyFromUTxo(oUtxo);
+
+            if (publicKey == null) {
+                return false;
+            }
 
             byte[] message = tx.getRawDataToSign(i);
             byte[] signature = input.signature;
             //rule number 2
-//            if (!Crypto.verifySignature(publicKey, message, signature)) {
-//              return false;
-//            }
+            if (!Crypto.verifySignature(publicKey, message, signature)) {
+                return false;
+            }
 
             //rule number 3
-            if (usedUTXO.containsKey(utxo)) { return false; }
+            if (usedUTXO.containsKey(utxo)) {
+                return false;
+            }
 
             usedUTXO.put(utxo, true);
 
@@ -75,13 +85,32 @@ public class TxHandler {
         return true;
     }
 
+    private PublicKey getPublicKeyFromUTxo(Optional<UTXO> oUtxo) {
+        byte[] bytes = oUtxo.get().getAddress();
+        PublicKey publicKey = null;
+        try {
+            publicKey = getRsaPublicKeyKeyFromBytes(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return publicKey;
+    }
+
+    public static PublicKey getRsaPublicKeyKeyFromBytes(byte[] bytes) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        return KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(bytes));
+    }
+
     private boolean validateRuleNumber4And5(Transaction tx) {
         double outputSum = 0;
 
-        for (int i = 0;  i < tx.numOutputs(); i++) {
+        for (int i = 0; i < tx.numOutputs(); i++) {
             Transaction.Output output = tx.getOutput(i);
-            if (output == null) { return false; }
-            if (output.value < 0) { return false; }
+            if (output == null) {
+                return false;
+            }
+            if (output.value < 0) {
+                return false;
+            }
 
             outputSum += output.value;
         }
@@ -123,5 +152,5 @@ public class TxHandler {
 
         return validTxs.toArray(new Transaction[validTxs.size()]);
     }
-    
+
 }
