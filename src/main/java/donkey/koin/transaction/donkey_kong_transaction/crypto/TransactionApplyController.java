2160456d27cd4253d489ca6d1b76b228f8b1f473
sowakarol <karosowa@student.agh.edu.pl>
2018-06-17 21:38:26 +0200

    getMyTransactions function added

--- src/main/java/donkey/koin/transaction/donkey_kong_transaction/crypto/TransactionApplyController.java
+++ src/main/java/donkey/koin/transaction/donkey_kong_transaction/crypto/TransactionApplyController.java
@@ -3,10 +3,13 @@ package donkey.koin.transaction.donkey_kong_transaction.crypto;
 import donkey.koin.transaction.donkey_kong_transaction.koin.KoinManager;
 import donkey.koin.transaction.donkey_kong_transaction.repo.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static donkey.koin.transaction.donkey_kong_transaction.crypto.TransactionApplyController.TRANSACTION_ENDPOINT;
@@ -37,7 +40,24 @@ public class TransactionApplyController {
 
     @RequestMapping(method = GET)
     List<Transaction> getAllTransactions() {
-        return this.transactionRepository.findAll();
+        List<Transaction> transactions = this.transactionRepository.findAll();
+        System.out.println(transactions);
+        return transactions;
+    }
+
+    @RequestMapping(method = GET, value = "/{publicKey}")
+    List<Transaction> getMyTransactions(@PathVariable byte[] publicKey) {
+        List<Transaction> transactions = this.transactionRepository.findAll();
+        List<Transaction> myTransactions = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            for (Transaction.Output output : transaction.getOutputs()) {
+                if (Arrays.equals(output.getAddress(), publicKey)) {
+                    myTransactions.add(transaction);
+                    break;
+                }
+            }
+        }
+        return myTransactions;
     }
 
 }
